5957905d0ba1da2ff6ce378143364e19d7c1960a
shaqcoleman <shaquille.coleman3@gmail.com>
2024-11-18 14:47:08 -0500

    Add files via upload

---
+++ Module1_Excercise1_1.java
@@ -0,0 +1,6 @@
+public class Module 1: Exercise 1.1{
+    
+public static void main(String []args){
+    System.out.println("Welcome to Java", "Welcome to Computer Science", "Programming is fun.");
+    }
+}

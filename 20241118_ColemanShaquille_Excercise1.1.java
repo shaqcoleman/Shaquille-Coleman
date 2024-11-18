077ea2127c78cf471804a8ce56acbd7e1a4f0b99
shaqcoleman <shaquille.coleman3@gmail.com>
2024-11-18 15:17:52 -0500

    Delete 20241118_ColemanShaquille_Excercise1.1.java

--- 20241118_ColemanShaquille_Excercise1.1.java
+++
@@ -1,7 +0,0 @@
-public class Module1_Excercise1_1 {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Java");
-        System.out.println("Welcome to Computer Science");
-        System.out.println("Programming is fun.");
-    }
-}

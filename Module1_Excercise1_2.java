df2bea9abc67ff3e0d141a862fdb86269fe69f95
shaqcoleman <shaquille.coleman3@gmail.com>
2024-11-20 07:09:06 -0500

    Delete Module1_Excercise1_2.java

--- Module1_Excercise1_2.java
+++
@@ -1,9 +0,0 @@
-public class Module1_Excercise1_2 {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Java");
-        System.out.println("Welcome to Java");
-        System.out.println("Welcome to Java");
-        System.out.println("Welcome to Java");
-        System.out.println("Welcome to Java");
-    }
-}

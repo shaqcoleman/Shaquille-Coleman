779f65335c437698aaca0e6e6861dcbc6289d292
shaqcoleman <shaquille.coleman3@gmail.com>
2024-11-20 07:00:32 -0500

    Add files via upload

---
+++ Module1_Excercise1_2.java
@@ -0,0 +1,9 @@
+public class Module1_Excercise1_2 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
